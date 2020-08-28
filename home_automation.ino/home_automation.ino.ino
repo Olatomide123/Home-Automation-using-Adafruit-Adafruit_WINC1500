891f73bc8d587ff76af3e81b78de6c36bb0f4aa0
Olatomide123 <50994770+Olatomide123@users.noreply.github.com>
2020-08-28 03:41:05 +0200

    First Commit

---
+++ home_automation.ino/home_automation.ino.ino
@@ -0,0 +1,147 @@
+#include <SPI.h>
+#include <Adafruit_WINC1500.h>
+
+// The WINC1500 board connections is shown below
+// If you're following the Adafruit WINC1500 board, guide you don't need to modify these:
+ 
+#define WINC_CS   8
+#define WINC_IRQ  7
+#define WINC_RST  4
+#define WINC_EN   2     // or, tie EN to VCC
+
+// The SPI pins of the WINC1500 (SCK, MOSI, MISO) should be connected to the hardware SPI port of the Arduino.
+// On an Uno or compatible these are SCK = #13, MISO = #12, MOSI = #11.
+
+
+// Setup the WINC1500 connection with the pins above and the default hardware SPI.
+Adafruit_WINC1500 WiFi(WINC_CS, WINC_IRQ, WINC_RST);
+
+
+#define LED_PIN  13  
+#define FAN_PIN  12
+
+char ssid[] = "Tomide";      //  created AP name, replace this with your AP name and password
+char pass[] = "olatomide";        
+
+int status = WL_IDLE_STATUS;
+Adafruit_WINC1500Server server(80);
+
+void setup() {
+#ifdef WINC_EN
+  pinMode(WINC_EN, OUTPUT);
+  digitalWrite(WINC_EN, HIGH);
+#endif
+
+  while (!Serial);
+  delay(1000);
+  Serial.begin(9600);      // initialize serial communication
+
+  Serial.println("Access Point Web Server");
+
+  pinMode(LED_PIN, OUTPUT);      // set the LED pin mode
+  pinMode(FAN_PIN, OUTPUT);      // set the fan pin mode
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);       
+  }
+
+  
+  Serial.print("Creating Network named: ");
+  Serial.println(ssid);                   // print the network name (SSID);
+  status = WiFi.beginAP(ssid);
+  // wait 10 seconds for connection:
+  delay(10000);
+  server.begin();    // start the web server on port 80
+ printWifiStatus();
+ 
+ 
+}
+
+
+void loop() {
+  Adafruit_WINC1500Client client = server.available();   // listen for incoming clients
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>"); 
+            client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+            client.print("Click <a href=\"/A\">here</a> turn the FAN on<br>");
+            client.print("Click <a href=\"/B\">here</a> turn the FAN off<br>");
+            
+
+           
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(LED_PIN, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(LED_PIN, LOW);                // GET /L turns the LED off
+        }
+        if (currentLine.endsWith("GET /A")) {
+          digitalWrite(FAN_PIN, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /B")) {
+          digitalWrite(FAN_PIN, LOW);               // GET /H turns the LED on
+        }
+      }
+    }
+         
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+ 
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+}
